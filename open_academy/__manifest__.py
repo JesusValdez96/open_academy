bf0d2652bada952e535a4609ddda468e7ccf3b0a
Jesus Feliciano Valdez Cruz <jvaldez@vauxoo.com>
2022-07-20 23:48:58 -0500

    Draft: [ADD] open_academy: Creation of the module T#59081  Exercise "Module creation":  Use of the scaffold command to create an empty module.

---
+++ open_academy/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': "Open Academy",
+
+    'summary': "Creation of the open academy module.",
+
+    'description': "Exercise of Building a module",
+
+    'author': "Jesús Valdez [Vauxoo]",
+    'website': "https://github.com/JesusValdez96/open_academy",
+
+    'category': 'Uncategorized',
+    'version': '15.0.1.0.0',
+
+    'depends': ['base'],
+
+    'data': [
+        'security/ir.model.access.csv'
+    ],
+
+    'demo': [],
+}
