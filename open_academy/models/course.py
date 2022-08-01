fc056da34c64f3fbc069fe389493152e34a99b6c
Jesús Feliciano Valdez Cruz [Vauxoo] <jvaldez@vauxoo.com>
2022-08-01 15:00:55 -0500

    [FIX] open_academy: Fix course name displayed T#59081 - Change title field to name field in course model, course view and demo data to correctly display the name of the courses.

--- open_academy/models/course.py
+++ open_academy/models/course.py
@@ -5,7 +5,7 @@ class Course(models.Model):
     _name = 'course'
     _description = 'Open Academy Course'
 
-    title = fields.Char(required=True)
+    name = fields.Char(required=True)
     description = fields.Text()
     responsible_user = fields.Many2one("res.users")
     sessions = fields.One2many("session", "course")
