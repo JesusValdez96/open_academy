3b8e82d90d1b2b7f2447fd3dd013e914f6316111
Jesús Feliciano Valdez Cruz [Vauxoo] <jvaldez@vauxoo.com>
2022-08-02 12:32:00 -0500

    [FIX] open_academy: Fix relational field names T#59081 - Fix Many2one, One2many and Many2many fields name in course and session model according to OCA guidelines. - Modify the session and course views to match the new fields

--- open_academy/models/course.py
+++ open_academy/models/course.py
@@ -7,5 +7,9 @@ class Course(models.Model):
 
     name = fields.Char(required=True)
     description = fields.Text()
-    responsible_user = fields.Many2one("res.users")
-    sessions = fields.One2many("session", "course")
+    responsible_user_id = fields.Many2one("res.users")
+    session_ids = fields.One2many(
+        "session",
+        "course_id",
+        string="Sessions"
+    )
