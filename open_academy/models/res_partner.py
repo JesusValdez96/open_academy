19da8e10939283ca28a47ad25cc30a84b0e69adc
Jesús Feliciano Valdez Cruz [Vauxoo] <jvaldez@vauxoo.com>
2022-08-03 14:04:51 -0500

    [IMP] open_academy: Add instructor and teacher domain T#59081 - Add teacher_level field in res_partner model to add a selection of teacher level to the partner view. - Add domain to instructor_id field at session model to display only partners that are instructors or teachers in session form view. Link to exercise: https://www.odoo.com/documentation/15.0/developer/howtos/backend.html#domains

--- open_academy/models/res_partner.py
+++ open_academy/models/res_partner.py
@@ -6,3 +6,7 @@ class Partner(models.Model):
 
     is_instructor = fields.Boolean()
     session_ids = fields.Many2many("session")
+    teacher_level = fields.Selection([
+        ("1", "Level 1"),
+        ("2", "Level 2"),
+    ])
